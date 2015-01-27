69c66ee6b65484282dfa52acda2aae8fe3d7d50e
Thomas Deutsch <deutsch@init7.net>
2015-01-27 10:31:43 +0100

    updated settings

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
-AUTHOR = "Mathieu Leplatre"
-SITEURL = "http://blog.mathieu-leplatre.info"
-SITENAME = "Mathieu Leplatre"
+AUTHOR = "Thomas Deutsch"
+SITEURL = "http://test.thomas-deutsch.ch"
+SITENAME = "Thomas Deutsch"
 LOCALE = 'en_US.utf8'
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Zurich'
 
 DISPLAY_PAGES_ON_MENU = True
 WITH_PAGINATION = True
@@ -12,19 +12,16 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
 
-TWITTER_USERNAME = "leplatrem"
-GITHUB_URL = "https://github.com/leplatrem"
-DISQUS_SITENAME = "mathieuleplatre"
-PIWIK_URL = "mathieu-leplatre.info/piwik"
-PIWIK_SITE_ID = 1
+TWITTER_USERNAME = "tdeutsch"
+GITHUB_URL = "https://github.com/tuxpeople"
+#DISQUS_SITENAME = "mathieuleplatre"
+PIWIK_URL = "http://stat.thomas-deutsch.ch/"
+PIWIK_SITE_ID = 4
 
 SOCIAL = (
-    ("LinkedIn", "http://www.linkedin.com/in/leplatre"),
-    ("Launchpad", "https://code.launchpad.net/~mathieu.leplatre"),
-    ("GitHub", "https://github.com/leplatrem"),
-    ("Google+", "https://plus.google.com/u/0/106965745149150173373"),
-    ("Twitter", "http://twitter.com/leplatrem"),
-    ("Identi.ca", "http://identi.ca/leplatrem"),
+        ("LinkedIn", "http://ch.linkedin.com/pub/thomas-deutsch/39/a18/844"),
+    ("GitHub", "https://github.com/tuxpeople"),
+    ("Twitter", "http://twitter.com/tdeutsch"),
 )
 
 STATIC_PATHS = ['images', 'extra/CNAME']
